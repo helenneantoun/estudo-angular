f21a352d9ea75ca3175f8d9962070c822413bcfe
helenneantoun <helennerantoun@hotmail.com>
2023-12-08 13:17:17 -0300

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ComponentcabecalhoComponent } from './componentcabecalho/componentcabecalho.component';
+import { RodapeComponent } from './componente/rodape/rodape.component';
+import { CriarPensamentosComponent } from './componentes/pensamentos/criar-pensamentos/criar-pensamentos.component';
+import { CabecalhoComponent } from '2438-angular-memoteca/src/app/componentes/cabecalho/cabecalho.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ComponentcabecalhoComponent
+    ComponentcabecalhoComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    CriarPensamentosComponent,
   ],
   imports: [
     BrowserModule
