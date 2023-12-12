ccb6ddeb62df14e9099fddf9e76d5451f5fec2d9
helenneantoun <helennerantoun@hotmail.com>
2023-12-12 14:37:43 -0300

    arrumei acho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HomeComponent } from './home/home.component';
 import { ComponentcabecalhoComponent } from './componentcabecalho/componentcabecalho.component';
 import { RodapeComponent } from './componente/rodape/rodape.component';
 import { CriarPensamentosComponent } from './componentes/pensamentos/criar-pensamentos/criar-pensamentos.component';
-import { CabecalhoComponent } from '2438-angular-memoteca/src/app/componentes/cabecalho/cabecalho.component';
 import { FormsModule } from '@angular/forms';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 import { RouterModule, RouterOutlet } from '@angular/router';
@@ -19,7 +18,6 @@ import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensam
     AppComponent,
     HomeComponent,
     ComponentcabecalhoComponent,
-    CabecalhoComponent,
     RodapeComponent,
     CriarPensamentosComponent,
     ListarPensamentoComponent,
